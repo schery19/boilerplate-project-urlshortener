183a35657fee6679490033a9b6d0747d48937ea0
schery19 <schneiderchery7@gmail.com>
2024-10-09 04:21:21 +0000

    url shortener project

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
+const bodyParser = require('body-parser');
+const { parse } = require('url');
 const app = express();
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,14 +14,32 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+app.use(bodyParser.json());
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
+  const { originalUrl } = req.body;
+  const parsedUrl = parse(originalUrl);
+
+  dns.lookup(parsedUrl.hostname, (err) => {
+    if (err) {
+      return res.status(400).json({ error: 'Invalid URL' });
+    }
+
+    const shortUrl = generateShortUrl();
+    res.json({ originalUrl, shortUrl });
+  });
+
 });
+
+
+const generateShortUrl = () => Math.random().toString(36).substr(2, 5);
+
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
