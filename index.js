f7099b56dde5de06a4f431a50e4d198e0d5ef87a
schery19 <schneiderchery7@gmail.com>
2024-10-09 05:25:47 +0000

    urlshortener final version

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const dns = require('dns');
-const bodyParser = require('body-parser');
+const { LocalStorage } = require('node-localstorage');
+const localStorage = new LocalStorage('./scratch');
 const { parse } = require('url');
+
 const app = express();
 
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-app.use(cors());
 
+app.use(express.urlencoded());
+app.use(cors());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.use(bodyParser.json());
+
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -22,22 +26,50 @@ app.get('/', function(req, res) {
 
 // Your first API endpoint
 app.post('/api/shorturl', function(req, res) {
-  const { originalUrl } = req.body;
-  const parsedUrl = parse(originalUrl);
+  const { url } = req.body;
 
-  dns.lookup(parsedUrl.hostname, (err) => {
-    if (err) {
+  if (!isValidHttpUrl(url)) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  const parsedUrl = parse(url);
+
+  dns.lookup(parsedUrl.hostname, (err, address) => {
+    if (err || !address) {
+      // Si l'URL n'est pas valide
       return res.status(400).json({ error: 'Invalid URL' });
     }
 
-    const shortUrl = generateShortUrl();
-    res.json({ originalUrl, shortUrl });
+    const short_url = generateShortUrl();
+    localStorage.setItem(short_url, url);
+
+    res.json({original_url: url, short_url });
   });
+
 
 });
 
 
-const generateShortUrl = () => Math.random().toString(36).substr(2, 5);
+app.get('/api/shorturl/:shortUrl', (req, res) => {
+  let original_url = localStorage.getItem(req.params.shortUrl);
+
+  if(original_url) 
+    res.redirect(original_url)
+  else
+    res.status(404).json({ error: 'URL not found' });
+})
+
+
+const generateShortUrl = () => Math.floor(Math.random() * 10000);
+
+function isValidHttpUrl(string) {
+  try {
+    const url = new URL(string);
+    return url.protocol === 'http:' || url.protocol === 'https:'; // Only allow HTTP/HTTPS URLs
+  } catch (_) {
+    return false;
+  }
+}
 
 
 
